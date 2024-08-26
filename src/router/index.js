daa267f5ae0abd3285aad4d6bad129d171a50f52
Roberto Maciel <roberto.maciel@cohros.com.br>
2024-08-26 17:15:28 -0300

    create contact view

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const router = createRouter({
       name: 'education',
       component: () => import('../views/EducationView.vue'),
     },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue'),
+    },
   ],
 });
 
---
+++ src/views/ContactView.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex flex-col md:flex-row md:space-x-4 space-y-2">
+    <div class="w-full">
+      <div class="mb-2">
+        <h1>Contato</h1>
+      </div>
+
+      <div class="text-justify">
+        <p>Para qualquer duvida, contato, ou sugestão, entre em contato:</p>
+
+        <p>WhatsApp: <a href="https://wa.me/5511999999999" class="font-bold underline" target="_blank">(11) 9 9999-9999</a></p>
+        <p>E-mail: <a href="mailto:Jpj4h@example.com" class="font-bold underline" target="_blank">Jpj4h@example.com</a></p>
+        <p>Instagram: <a href="https://www.instagram.com/maryanebedaque.di/" class="font-bold underline" target="_blank">@maryanebedaque.di</a></p>
+      </div>
+    </div>
+  </div>
+</template>
