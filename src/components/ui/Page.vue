ff8d493bb5aa5559d5630d6248bd8c6aac5a446f
Roberto Maciel <roberto.maciel@cohros.com.br>
2024-08-27 14:06:06 -0300

    some adjustments

--- src/components/ui/Page.vue
+++ src/components/ui/Page.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="min-h-screen">
-    <div class="py-4 px-2 md:px-32">
-      <div class="flex flex-col md:flex-row md:space-x-4 space-y-2">
+    <div class="py-4 px-2 md:px-60">
+      <div class="md:space-x-4 space-y-2">
         <slot></slot>
       </div>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,34 +4,34 @@ import Page from '../components/ui/Page.vue';
 
 <template>
   <page id="home">
-    <div class="w-full">
-      <img class="w-full rounded-3xl" src="/src/assets/mb1.jpg" alt="Maryane Bedaque" />
-    </div>
-
-    <div class="w-full">
-      <div class="mb-2">
-        <h1>Maryane Bedaque</h1>
-
-        <h3>Desenvolvedora Infantil</h3>
-      </div>
-
-      <div class="text-justify">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor,
-          vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient
-          montes, nascetur ridiculus mus. Integer sed lacus sit amet nisl eleifend malesuada. Sed
-          vitae erat auctor, aliquam massa ut, convallis nunc.
-        </p>
-        <p>
-          Sed ut lorem quis nisi vestibulum dignissim. Suspendisse et nisl in metus malesuada
-          ultricies. Nulla facilisi. Sed quis lectus porta, porta nisi eu, ultrices nunc. Nullam sed
-          justo nec erat malesuada tincidunt.
-        </p>
-        <p>
-          Sed quis nibh vitae velit malesuada facilisis. Morbi vitae nulla in massa tincidunt
-          tincidunt. Donec auctor, magna in faucibus pellentesque, massa nisl tincidunt dui, ut
-          malesuada lectus nunc vitae nunc. Sed vel nunc eu justo convallis malesuada.
-        </p>
+    <div class="w-full flex flex-col md:flex-row md:space-x-2">
+      <img class="rounded-3xl md:w-1/2 md:h-1/2" src="/src/assets/mb1.jpg" alt="Maryane Bedaque" />
+  
+      <div>
+        <div class="mb-2">
+          <h1>Maryane Bedaque</h1>
+  
+          <h3>Desenvolvedora Infantil</h3>
+        </div>
+  
+        <div class="text-justify">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor,
+            vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient
+            montes, nascetur ridiculus mus. Integer sed lacus sit amet nisl eleifend malesuada. Sed
+            vitae erat auctor, aliquam massa ut, convallis nunc.
+          </p>
+          <p>
+            Sed ut lorem quis nisi vestibulum dignissim. Suspendisse et nisl in metus malesuada
+            ultricies. Nulla facilisi. Sed quis lectus porta, porta nisi eu, ultrices nunc. Nullam sed
+            justo nec erat malesuada tincidunt.
+          </p>
+          <p>
+            Sed quis nibh vitae velit malesuada facilisis. Morbi vitae nulla in massa tincidunt
+            tincidunt. Donec auctor, magna in faucibus pellentesque, massa nisl tincidunt dui, ut
+            malesuada lectus nunc vitae nunc. Sed vel nunc eu justo convallis malesuada.
+          </p>
+        </div>
       </div>
     </div>
   </page>
