4d35eadb4b2f3f5116d5cba5c4f5d3f165251601
Roberto Maciel <roberto.maciel@cohros.com.br>
2024-08-27 14:23:01 -0300

    add the contact list in the app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+import { computed } from 'vue';
 import AboutView from './views/AboutView.vue';
 import ContactView from './views/ContactView.vue';
 import EducationView from './views/EducationView.vue';
 import HomeView from './views/HomeView.vue';
 import WorkExperienceView from './views/WorkExperienceView.vue';
+import FloatingButton from './components/ui/FloatingButton.vue';
+
+const isMobile = computed(() => window.innerWidth < 768);
 </script>
 
 <template>
@@ -12,6 +16,8 @@ import WorkExperienceView from './views/WorkExperienceView.vue';
     <about-view />
     <work-experience-view />
     <education-view />
-    <contact-view />
+    <contact-view v-if="!isMobile"/>
+
+    <floating-button />
   </div>
 </template>
